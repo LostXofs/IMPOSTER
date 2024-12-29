fb7a9b1c148c3e474c4a20526e4d77068c27b3d4
LostXofs <lostmmma@gmail.com>
2024-12-29 05:28:27 +0100

    Create IIM.js

---
+++ IIM.js
@@ -0,0 +1,13 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "Sensi imposters") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+window.alert(" 𝗘𝘅𝗽𝗶𝗿𝘀 𝗣𝗮𝗻𝗲𝗹 ( 𝗟𝗶𝗳𝗲 𝗧𝗶𝗺𝗲 ) ");
+
+window.alert(" 𝗪𝗔𝗟𝗖𝗢𝗠 𝗧𝗢 𝗣𝗔𝗡𝗘𝗟 𝗦𝗘𝗡𝗦𝗜 𝗜𝗠𝗣𝗢𝗦𝗧𝗘𝗥𝗦 ");
+
+ }
